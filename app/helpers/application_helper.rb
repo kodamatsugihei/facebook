3c224ae08f1948cb6f637b2e0ea3bc7d189c1804
kodama <javajava552000@yahoo.co.jp>
2017-06-20 12:06:28 +0900

    change storage to fog from file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,16 @@
 module ApplicationHelper
   def profile_img(user)
     return image_tag(user.avatar, alt: user.name) if user.avatar?
-    
+
     unless user.provider.blank?
       img_url = user.image_url
     else
       img_url = 'no_image.png'
     end
     image_tag(img_url, alt: user.name)
+  end
+
+  def active?(controller_name)
+    return "active" if controller_name == params[:controller]
   end
 end
